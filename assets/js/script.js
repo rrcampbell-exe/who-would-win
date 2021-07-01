0edd894122544f8ade073807528478127b0b1f35
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-07-01 15:29:21 -0500

    Update with Pokemon attribute functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,6 @@
                     .then(res => res.json())
                     .then(data => { 
                         let dSpeed = data.speed
-                        console.log(dSpeed)
                         // convert speed of d&d character to pokemon equivalent, with max d&d character speed = 100
                         if (dSpeed == 25) {
                             let dSpeedAdj = Math.ceil(Math.random() * dSpeed) + 45
@@ -50,7 +49,7 @@
                         .then(data => {
                             let dLevel = Math.ceil(Math.random() * 5)
                             console.log(dLevel)
-                            let hpTotal = dLevel * data.hit_die
+                            let hpTotal = dLevel * data.hit_die // currently, hit points are assigned assuming max roll of all hit point dice
                             console.log(hpTotal)
                         })
                 // establish weapon or spell selection based on class
@@ -59,7 +58,7 @@
                     fetch(apiAttackChoice)
                         .then(res => res.json())
                         .then(data => {
-                            let randomIndex = Math.floor(Math.random() * (data.equipment.length - 1))
+                            let randomIndex = Math.ceil(Math.random() * (data.equipment.length - 1))
                             let weapon = data.equipment[randomIndex].name.toLowerCase()
                             console.log(weapon);
                         })
@@ -78,10 +77,53 @@
     };
 
     // establish pokemon from gen 1
+    function pokeStats () {
+        var randomPoke = Math.ceil(Math.random() * 151)
+        let pokeApi = `https://pokeapi.co/api/v2/pokemon/${randomPoke}/`
+        fetch(pokeApi)
+            .then(res => res.json())
+            .then(data => {
+                let pokeChoice = data.name
+                console.log(pokeChoice)
+            // establish pokemon hp
+            let chosenPokeApi = "https://pokeapi.co/api/v2/pokemon/" + pokeChoice + "/"
+            fetch(chosenPokeApi)
+                .then(res => res.json())
+                .then(data => {
+                    let pokeHp = data.stats[0].base_stat
+                    console.log(pokeHp);
+                })
+            // establish pokemon speed
+            fetch(chosenPokeApi)
+                .then(res => res.json())
+                .then(data => {
+                    let pokeSpeed = data.stats[5].base_stat
+                    console.log(pokeSpeed)
+                })
+            // establish pokemon move of choice
+            fetch(chosenPokeApi)
+                .then(res => res.json())
+                .then(data => {
+                    let randomIndex = Math.ceil(Math.random() * (data.moves.length))
+                    let pokeMove = data.moves[randomIndex].move.name
+                    console.log(pokeMove);  
+                // establish power of pokemon's move of choice
+                let chosenMoveApi = data.moves[randomIndex].move.url 
+                fetch(chosenMoveApi)
+                    .then(res => res.json())
+                    .then(data => {
+                        let pokeMovePower = data.power
+                        console.log(pokeMovePower);
+                        if (pokeMovePower == null) {
+                            pokeStats(); // don't do this long-term; need to un-nest fetches and create separate functions so you can call the chosenMove function again here
+                        }
+                    })
+                })
 
-    // establish pokemon hp
+            })
 
-    // establish pokemon moveset
+    }
+
 
 // FUNCTIONS FOR BATTLE
 
@@ -90,3 +132,4 @@
 // RUNNING OF FUNCTIONS ON PAGE LOAD
 dRace();
 dClassEtAl();
+pokeStats();
